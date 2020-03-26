b1cebe96a48bce2ef13f0b215223a641839cef55
Ghirmawit <ghirmawiande@gmail.com>
2020-03-25 20:20:27 -0400

    update

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -1,7 +1,12 @@
 public class FirstClass {
     public static void main(String[] args) {
+
         System.out.println("Hello world");
         System.out.println("Hi beautiful");// commit
+
+        System.out.println("Hello world");// commit
+        System.out.println("Hi beautiful");
+
         System.out.println("HELLO");
         System.out.println("HELLO Again");
         System.out.println("HELLO Again Again");
